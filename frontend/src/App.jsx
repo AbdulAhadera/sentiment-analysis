9eef425451d207108c830267bb6d5f82109a57a9
AbdulAhadera <abdulahad.era@gmail.com>
2025-02-11 20:21:42 +0500

    "Main Page Completed"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,30 @@
-import { BrowserRouter } from "react-router-dom";
+import { useState } from 'react';
 import Router from './Router';
+import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import MainPage from './pages/MainPage';
 
 function App() {
+  const [mainPage, setMainPage] = useState(true)
 
   return (
     <BrowserRouter>
-      <Navbar />
-      <div>
-        <Router />
-      </div>
-      <Footer />
+      {mainPage ?
+        <>
+          <>
+            <MainPage />
+          </>
+        </>
+        :
+        <>
+          <Navbar />
+          <div className='px-2 md:px-10'>
+            <Router />
+          </div>
+          <Footer />
+        </>
+      }
     </BrowserRouter>
   )
 }
--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,9 +1,44 @@
-
+import { WavyBackground } from "../components/ui/wavy-background.jsx";
+import { PlaceholdersAndVanishInput } from "../components/ui/placeholders-and-vanish-input.jsx"
 
 function MainPage() {
+
+  // Input UI Functionality
+
+  const placeholders = [
+    "Review a product ?",
+    "Customer feedbacks drives innovation?",
+    "Shaping Better Products with Every Word?"
+  ];
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log("submitted");
+  };
+
+
   return (
-    <div>MainPage</div>
+    <>
+      <WavyBackground className="max-w-4xl mx-auto ">
+        <p className="text-5xl md:text-6xl lg:text-8xl black font-bold inter-var text-center ">
+          Critque AI
+        </p>
+        <p className="text-base md:text-lg mt-4 text-black font-normal inter-var text-center pb-28">
+          Analyze. Improve. Succeed.
+        </p>
+        <div className="">
+        <PlaceholdersAndVanishInput
+            placeholders={placeholders}
+            onChange={handleChange}
+            onSubmit={onSubmit}
+          />
+        </div>
+      </WavyBackground>
+    </>
   )
 }
 
-export default MainPage
+export default MainPage;
